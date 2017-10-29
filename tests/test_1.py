f41ae6c9223704a1cbbed99e7ae2709233098915
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-10-29 15:06:16 +0300

    Create models.py and tests folder Create models.py for the classes Create tests folder for running tests on a specific class

---
+++ tests/test_1.py
@@ -0,0 +1,17 @@
+import unittest
+# Import class to be tested
+# from app.models import [Class]
+
+class Test(unittest.TestCase):
+    '''
+    Test class to test behaviours of the [Class] class
+
+    Args:
+        unittest.TestCase : Test case class that helps create test cases
+    '''
+
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        pass
