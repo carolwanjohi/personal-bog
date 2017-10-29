95172b8a800bdccb5109bde555879aa9975960e4
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-10-29 14:38:30 +0300

    Set up launch file Set up launch file for running the application

---
+++ manage.py
@@ -0,0 +1,40 @@
+# Import db from app factory
+from app import create_app
+from flask_script import Manager,Server
+# Connect to models
+# from app.models import User,Role,Review
+# Set up migrations
+# from flask_migrate import Migrate,MigrateCommand
+
+# Creating app instance
+# app = create_app('test')
+app = create_app('development')
+# app = create_app('production')
+
+
+# Create manager instance 
+manager = Manager(app)
+
+# Create migrate instance
+# migrate = Migrate(app,db)
+
+manager.add_command('server',Server)
+# manager.add_command('db',MigrateCommand)
+
+@manager.command
+def test():
+    '''
+    Run the unit tests
+    '''
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+
+# @manager.shell
+# def make_shell_context():
+#     return dict( app=app, db=db, User=User, Review=Review, Role=Role)
+
+
+if __name__ == '__main__':
+    manager.run()
