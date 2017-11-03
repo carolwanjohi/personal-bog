f5c162d7f43d899ed01efd3daea4d8539ddf008c
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-11-03 12:47:21 +0300

    Set Up Auth Blueprint Create app/auth and its __init__.py to initialise it as a blueprint Connent auth blueprint with the application factory

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,17 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
-# from flask_sqlalchemy import SQLAlchemy
-# from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 
 
 # Instances of flask extensions
-# Instance of LoginManger and using its methods
-# login_manager = LoginManager()
-# login_manager.session_protection = 'strong'
-# login_manager.login_view = 'auth.login'
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 bootstrap = Bootstrap()
-# db = SQLAlchemy()
+db = SQLAlchemy()
 
 def create_app(config_name):
     '''
@@ -31,16 +30,16 @@ def create_app(config_name):
 
     # Initialising flask extensions
     bootstrap.init_app(app)
-    # db.init_app(app)
-    # login_manager.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
 
     # Regestering the main blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
     # Regestering the auth bluprint
-    # from .auth import auth as auth_blueprint
-    # app.register_blueprint(auth_blueprint, url_prefix='/auth')
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
     # Setting config when using an API
     # from .requests import configure_request
