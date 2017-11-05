164b018b7e99951ec76af1e837e349f08aa78cec
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-11-05 12:15:29 +0300

    Create subscribe column Create subscribe column with default value as False Create test case for subcribe_user method of the User class Create test case for the get_subscribers method of the User class Create migration to a schema with users table containing subscribe column

--- app/models.py
+++ app/models.py
@@ -61,6 +61,9 @@ class User(UserMixin,db.Model):
     # relationship between user and comment class
     comments = db.relationship('Comment', backref='user', lazy='dynamic')
 
+    # subscribe column for user to subcribe to new post updates
+    subscribe = db.Column(db.Boolean, default=False)
+
     @property
     def password(self):
         raise AttributeError('You cannot read the password attribute')
@@ -87,6 +90,32 @@ class User(UserMixin,db.Model):
         '''
         db.session.add(self)
         db.session.commit()
+
+    @classmethod
+    def subcribe_user(cls,user_id):
+        '''
+        Function that queries the database and set subscribe to True for a user with the specified id
+
+        Args:
+            user_id : specific user id
+        '''
+        user = User.query.filter_by(id=user_id).update({
+            'subscribe':True
+            })
+        db.session.commit()
+
+    @classmethod
+    def get_subscribers(cls):
+        '''
+        Function that queries the Users Table in the database and returns only users who have subscribe set to True
+
+        Returns:
+            users : all the information for users who have subscribe set to True
+        '''
+        users = User.query.filter_by(subscribe=True).all()
+        return users
+
+
 
 class Post(db.Model):
     '''
