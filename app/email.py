007249cc953a4dc59462e5fbfb3cabffe4dba8e8
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-11-06 08:08:41 +0300

    Update subscription Update subscription to work when multiple users subscribe for new post updates

--- app/email.py
+++ app/email.py
@@ -6,7 +6,7 @@ subject_pref = 'the C blog'
 sender_email = 'wanjohi.carol75@gmail.com'
 
 def mail_message(subject,template,to,**kwargs):
-    email = Message(subject_pref+subject,sender=sender_email,recipients= [user for user in to.split()])
+    email = Message(subject_pref+subject,sender=sender_email,recipients= [user for user in to.split(',')])
     email.body = render_template(template + ".txt",**kwargs)
     email.html = render_template(template + ".html",**kwargs)
     mail.send(email)
